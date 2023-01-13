f4ce589734b15049311e6add25b826e8b87b7e2a
seb4storres <torresjuansebas@gmail.com>
2023-01-13 16:00:44 -0700

    Se agrega el proyecto

---
+++ MonolegalApi/Models/Cliente.cs
@@ -0,0 +1,36 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace MonolegalApi.Models
+{
+    public class Cliente
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonElement("name")]
+        public string Name { get; set; } = null!;
+
+        [BsonElement("CodigoFactura")]
+        public string CodigoFactura { get; set; } = null!;
+
+        [BsonElement("Ciudad")]
+        public string Ciudad { get; set; } = null!;
+
+        [BsonElement("TotalFactura")]
+        public int TotalFactura { get; set; } 
+
+        [BsonElement("Subtotal")]
+        public int Subtotal { get; set; } 
+
+        [BsonElement("Iva")]
+        public int Iva { get; set; } 
+
+        public string Estado { get; set; } = null!;
+
+        public bool Pagada { get; set; } = false;
+
+
+    }
+}
---
+++ MonolegalApi/Models/MonolegalDatabaseSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace MonolegalApi.Models
+{
+    public class MonolegalDatabaseSettings
+    {
+        public string ConnectionString { get; set; } = null!;
+
+        public string DatabaseName { get; set; } = null!;
+
+        public string ClientesCollectionName { get; set; } = null!;
+    }
+}
