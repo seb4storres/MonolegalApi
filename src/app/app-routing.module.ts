d8d593a37b9b3ab714d504805358ef7cddff0491
seb4storres <torresjuansebas@gmail.com>
2023-01-29 18:14:04 -0700

    Seagrega Angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FacturaComponent } from './factura/factura.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'factura',component: FacturaComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FacturaComponent } from './factura/factura.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MatTableModule} from '@angular/material/table'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FacturaComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    HttpClientModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
